f51382dd0992629c883ffa2b5743cc8842170571
Poletansky Viktor <poletansky@gmail.com>
2018-02-23 17:35:17 +0300

    gradle testing test

--- src/main/java/com/rprtr258/Main.java
+++ src/main/java/com/rprtr258/Main.java
@@ -1,4 +1,10 @@
+package com.rprtr258;
+
 public class Main {
+    public static int sqr(int x) {
+        return x * x + 1;
+    }
+
     public static void main(String[] args) {
         System.out.print("Hello, rukozhop\n");
     }
---
+++ src/test/java/com/rprtr258/MainTest.java
@@ -0,0 +1,12 @@
+package com.rprtr258;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+    @Test
+    public void sqrTest() {
+        assertEquals(25, Main.sqr(5));
+    }
+}
