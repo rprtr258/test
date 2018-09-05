d61b6641276bb4c0895bb9fbfd19c28069f46f7b
Poletansky Viktor <poletansky@gmail.com>
2018-09-05 15:23:15 +0300

    upd

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-let Phrase = require("rprtr258palindrome")
+let Phrase = require("rprtr258palindrome");
 
-alert(new Phrase("Madam, I'm Adam.").palindrome())
+let string = prompt("Please enter a string for palindrome testing:");
+let phrase = new Phrase(string);
+if (phrase.palindrome())
+    alert(`"${string}" is a palindrome!`);
+else
+    alert(`"${string}" is not a palindrome!`);
