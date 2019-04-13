26a8df98ffc17f01140fcf79ea1a80e60ddc2cc1
Viktor Poletansky <poletansky@gmail.com>
2019-04-13 11:52:19 +0300

    Add files via upload

---
+++ javadir/UnicodeMethodName.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(приветМир());
+    }
+    
+    private static String приветМир() {
+    	return "Hello, World!";
+    }
+}
