01f2cffd1a312be5f573344e1bb17265f1558b51
Poletansky Viktor <poletansky@gmail.com>
2019-07-02 16:27:04 +0300

    F

--- javadir/SyntaxError.java
+++
@@ -1,9 +0,0 @@
-import java.util.Arrays;
-
-public class Main {
-    public static void main(String[] args) {
-        String[] strings = Arrays.stream(new String[]{"ab", "bd", "ce"}).toArray(String[]::new);
-        for (String str : strings)
-            System.out.println(str);
-    }
-}
--- javadir/UnicodeComments.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-	// комментарий на иностранном языке
-	// 大众点评网网友｜寂m的男孩*上传的，昂舒巴黎中的。
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
